65ffed30b4bb8c23f72ba7bb9ca669ec30602b6a
sami-aloosi <kingxsam@gmail.com>
2021-06-10 21:01:16 -0700

    adding meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,8 @@ class MyDocument extends Document {
           <link rel="icon" href="/images/Logo.svg" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
           <meta name="author" content="Sami Al Aloosi"></meta>
+          <meta name="description" content="Sami Al Aloosi Portfolio "></meta>
+          <meta property="og:image" content="/images/demo.png"></meta>
           
         </Head>
         <body>
